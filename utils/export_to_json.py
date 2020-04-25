451a2a52b2a28f4274b8493922c4b7dfc64d5503
HJEGeorge <hjegeorge@gmail.com>
2020-04-25 18:17:38 +0100

    Added a tracks directory and added all contents to gitignore so that we dont checkin tracks

--- utils/export_to_json.py
+++ utils/export_to_json.py
@@ -56,6 +56,6 @@ def track_to_json(track: Track, filename=None):
 
 
 def create_json(filename: str="example", json_content: str= "{}"):
-    with open(f"{filename}.json", 'w+') as file:
+    with open(f"tracks/{filename}.json", 'w+') as file:
         file.write(json_content)
     return file.name
