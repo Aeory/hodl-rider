31c4708d8216c03340f488a7082d0c93928d714a
HJEGeorge <hjegeorge@gmail.com>
2020-04-25 14:31:02 +0100

    Added some basic models in models.py

---
+++ models.py
@@ -0,0 +1,40 @@
+import math
+from dataclasses import dataclass
+from datetime import date
+from typing import List
+
+
+class Track:
+    """Total track, including scales and date ranges.
+
+    Attributes:
+        start_date: datetime
+        end_date: datetime
+        lines: List of lines
+
+    """
+
+    def __init__(self, start_date: date, end_date: date, x_scale: float=1, y_scale: float=0.001):
+        self.start_date = start_date
+        self.end_date = end_date
+        self.x_scale = x_scale
+        self.y_scale = y_scale
+        self.lines: List[Line] = list()
+
+@dataclass
+class Line:
+    """A single line segment, connecting two points on a track
+
+    Attributes:
+        point_a: tuple of (x, y)
+        point_b: tuple of (x, y)
+
+    Methods:
+        len(line) returns the length of the line segment from point_a to point_b
+
+    """
+    point_a: tuple
+    point_b: tuple
+
+    def __len__(self):
+        return math.sqrt((self.point_b[1] - self.point_a[1])**2 + (self.point_b[1] - self.point_a[1])**2)
