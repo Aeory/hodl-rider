787b6dc7ef8f7577331064ebf296cbf1a9b0936e
Alex Burbidge <11243987+Aeory@users.noreply.github.com>
2020-04-27 14:44:29 +0100

    Create cache folder if it doesn't exist

--- utils/cache.py
+++ utils/cache.py
@@ -7,6 +7,12 @@ def json_cache(cache_directory: str):
     def json_cache_decorator(getter_func):
         @wraps(getter_func)
         def json_cached_getter(*args, **kwargs):
+            try:
+                os.makedirs(f"{cache_directory}/cache/")
+            except FileExistsError:
+                # directory already exists
+                pass
+
             arguments = list(args) + list(kwargs.values())
             call = '-'.join(map(str, arguments))
             cache_name = f'{cache_directory}/cache/{call}.json'
