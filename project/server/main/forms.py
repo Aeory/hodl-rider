d9a630a4e36528b7a728ee326a902e5bf6413116
HJEGeorge <hjegeorge@gmail.com>
2020-05-02 16:01:17 +0100

    Added validation to form front end and refactored text into the form in python

--- project/server/main/forms.py
+++ project/server/main/forms.py
@@ -7,11 +7,48 @@ from wtforms.validators import DataRequired, Optional
 
 
 class HodlForm(FlaskForm):
-    ticker = StringField("Stock tracker", [DataRequired()])
-    from_date = DateField("From", format="%d/%m/%Y", validators=[Optional()])
-    to_date = DateField("Till", format="%d/%m/%Y", validators=[Optional()])
-    smoothing = IntegerField(validators=[Optional()])
-    starting_acceleration = IntegerField(validators=[Optional()])
-    acceleration_chance = FloatField(validators=[Optional()])
-    y_scale = FloatField(validators=[Optional()])
-    x_scale = FloatField(validators=[Optional()])
+    ticker = StringField(
+        label="ticker symbol",
+        validators=[DataRequired()],
+        default='BTCUSD',
+        description="the stock you want to ride"
+    )
+    from_date = DateField(
+        label="from",
+        format="%d/%m/%Y",
+        validators=[Optional()],
+        description="if you leave either of these blank it will take the earliest / latest date available"
+    )
+    to_date = DateField(
+        label="to",
+        format="%d/%m/%Y",
+        validators=[Optional()]
+    )
+    smoothing = IntegerField(
+        label="track smoothhhhnessss",
+        validators=[Optional()],
+        description="1 is not an appropriate answer..."
+    )
+    x_scale = FloatField(
+        label="number of days per track piece (x-axis scale)",
+        validators=[Optional()],
+        description="reducing this makes the ride longer???"
+    )
+    y_scale = FloatField(
+        label="number of dollars per height (y-axis scale)",
+        validators=[Optional()],
+        description="big numbers for crypto advised.."
+    )
+    acceleration_chance = FloatField(
+        label="minimum acceleration chance",
+        validators=[Optional()],
+        description="the smallest chance that a piece of track will be an accelerator, results above .4 may result in orbit"
+    )
+    starting_acceleration = IntegerField(
+        label="starting acceleration tracks",
+        validators=[Optional()],
+        description="started from the bottom now we're here"
+    )
+
+
+
