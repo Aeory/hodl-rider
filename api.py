fc4f3d24c4709fad99aed1efd6f02579a0469b84
Alex Burbidge <11243987+Aeory@users.noreply.github.com>
2020-05-02 18:24:10 +0100

    Adds default configs for some tickers

--- api.py
+++ api.py
@@ -4,17 +4,47 @@ from dateutil import parser
 from export.export_to_json import track_to_json
 from datetime import date
 import settings
+from copy import deepcopy
+
+DEFAULT_CONFIG = {
+    'BTCUSD': {
+        'x_scale': 0.1, 'y_scale': -10.0
+    },
+    'GOOGL': {
+        'x_scale': 0.1, 'y_scale': -2.0
+    },
+    'AAPL': {
+        'x_scale': 0.1, 'y_scale': -0.4
+    },
+    'NFLX': {
+        'x_scale': 0.1, 'y_scale': -0.3
+    }
+}
 
 
-def hodl(ticker: str, start_date: date, end_date: date, config: dict = {}):
+def hodl(ticker: str, start_date: date, end_date: date, user_config: dict = None):
+    user_config = user_config or {}
+
+    config = deepcopy(
+        DEFAULT_CONFIG.get(
+            ticker,
+            {'x_scale': settings.DEFAULT_X_SCALE, 'y_scale': settings.DEFAULT_Y_SCALE}
+        )
+    )
+    for k, v in user_config.items():
+        if v is not None:
+            config[k] = v
+
     start_date_string = start_date.isoformat() if start_date else ""
     end_date_string = end_date.isoformat() if end_date else ""
     data = tiingo_client.get(start_date_string, end_date_string, ticker)
     start = data[0]
     start_date = parser.parse(start["date"]).date()
     end_date = parser.parse(data[-1]["date"]).date()
-    x_scale = config.get("x_scale") or settings.DEFAULT_X_SCALE
-    y_scale = config.get("y_scale") or settings.DEFAULT_Y_SCALE
+
+    x_scale = config["x_scale"]
+    y_scale = config["y_scale"]
+
     track = Track(
         start_date,
         end_date,
